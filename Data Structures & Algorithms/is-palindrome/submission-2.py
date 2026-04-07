40207789c6b39ecfc78b2cdbfc8fa7a5e4ff6740
Zoe (Zihan) Xia <120128884+ZoeCreates@users.noreply.github.com>
2026-04-06 17:55:30 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #create two pointers, one pointing to the beginning of the string and
+        # one to the end 
+        # iterate and compare each value the two characters pointing to 
+        l, r = 0, len(s)-1
+        
+        while l < r:
+            while l<r and not s[l].isalnum():
+                l+=1
+            while l < r and not s[r].isalnum():
+                r-=1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l+=1
+            r-=1
+        
+         
+        
+        return True 
+
+        
